94c9e2e9cebd49ad8c825907c5cb7f1eb532f337
micha003 <michael.vorogushyn@wbgym.de>
2025-01-02 16:11:29 +0100

    in between commit

--- frontend/src/tierlistmaker/src/scripts/file_upload.ts
+++ frontend/src/tierlistmaker/src/scripts/file_upload.ts
@@ -1,3 +1,18 @@
+function appendPicture(picture: File) {
+    const pictureBar = document.getElementById("pictureBar");
+
+    if (pictureBar) {
+        const imgElement = document.createElement("img");
+
+        imgElement.src = URL.createObjectURL(picture);
+        imgElement.className = "pictureBarIMG"
+
+        pictureBar.appendChild(imgElement);
+    } else {
+        console.error("Element not found.");
+    }
+}
+
 // This script runs when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", () => {
     const input = document.getElementById("upload") as HTMLInputElement;
@@ -11,6 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
             if (input.files && input.files.length > 0) {
                 const fileName = input.files[0].name;
                 fileNameDisplay.textContent = fileName;
+                appendPicture(input.files[0]);
             } else {
                 fileNameDisplay.textContent = "Keine Datei ausgewählt";
             }
